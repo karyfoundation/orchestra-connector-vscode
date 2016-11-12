573965442dcc9f73ed0c8988cdbdf0b8f9dfe804
Pouya Kary <k@karyfoundation.org>
2016-11-12 03:32:37 +0330

    wont work...

--- src/extension.js
+++ src/extension.js
@@ -8,6 +8,7 @@
 	const messenger 		= require('messenger');
 	const path 				= require('path');
 	const fs 				= require('fs');
+	const exec        		= require('child_process').exec
 
 //
 // ─── ACTIVATE EXTENSION ─────────────────────────────────────────────────────────
@@ -16,7 +17,7 @@
 	function activate ( context ) {
 		context.subscriptions.push(
 			vscode.commands.registerCommand(
-				'orchestra.open', openOrchestraBasedOnAddress )
+				'orchestra.open', openOrchestraBasedOnRegExp )
 		);
 	}
 
@@ -35,27 +36,25 @@
 
 	const orchestraClient = messenger.createSpeaker( 5994 );
 
-	function sendOpenRequest ( address, currentLine ) {
-		orchestraClient.request( 'open', address , response => {
-			switch ( response.kind ) {
-				default:
-
-			}
-		});
+	function sendOpenRequest ( regX ) {
+		let command = regX.replace( /"/g, '\\"' );
+		let fullCommand =
+			`"/Users/pmk/OneDrive/Codes/Orchestra/_release/Orchestra-darwin-x64/Orchestra.app/Contents/MacOS/Orchestra" "parse" "${ command }"`
+		exec( fullCommand, err => {
+            if ( err )
+			vscode.window.showInformationMessage('Could not launch Orchestra')
+        })
 	}
 
 //
 // ─── OPEN ORCHESTRA BASED ON ADDRESS ────────────────────────────────────────────
 //
 
-	function openOrchestraBasedOnAddress ( ) {
+	function openOrchestraBasedOnRegExp ( ) {
 		const currentLine = vscode.window.activeTextEditor.selection.active.line;
-		const address = getAddress( );
-		if ( existsSync( address ) ) {
-			sendOpenRequest( address );
-		} else {
-			vscode.window.showErrorMessage('The file does not exists');
-		}
+		const regExp = getRegExpFromLine( );
+		if ( regExp !== null | undefined | '' )
+			sendOpenRequest( regExp );
 	}
 
 //
@@ -66,73 +65,32 @@
 		return vscode.window.activeTextEditor.document.lineAt( lineNumber ).text;
 	}
 
-//
-// ─── GET ADDRESS ────────────────────────────────────────────────────────────────
-//
-
-	function getAddress ( ) {
-		// get address
-		const commentAddress = getAddressFromComment( );
-		if ( commentAddress === null ) return;
-
-		// resolve address
-		if ( existsSync( commentAddress ) ) {
-			return commentAddress;
-		} else {
-			return path.normalize(
-				path.join(
-					vscode.window.activeTextEditor.document.fileName
-						  .replace(/[^\/]*$/, ''),
-					commentAddress
-				)
-			);
-		}
-	}
 
 //
 // ─── GET ADDRESS ────────────────────────────────────────────────────────────────
 //
 
-	function getAddressFromComment ( ) {
+	function getRegExpFromLine ( ) {
 		// defs
-		const languageCommentSign = getCommentSign( );
 		const currentLineNumber = vscode.window.activeTextEditor.selection.active.line;
+		const line = getLineAt( currentLineNumber );
+		const regXFormula = /(?:\/[^\/]+\/|new(?:\s)+RegExp(?:\s)*\((?:\s)*(['"`])(?:\\\1|[^\1])+\1(?:\s)*\))/g;
 
-		// finding the comment line
-		var lineNum = currentLineNumber;
-		while ( true ) {
-			var line = getLineAt( lineNum );
-			if ( /^\s*\/\/\s*.+\.quartet\s*/.test( line ) ) {
-				return line.trim( ).replace(/^\s*\/\/\s*/, '');
+		// get regXFormula
+		const matches = regXFormula.exec( line );
 
-			} else if ( lineNum === 1 ) {
-				return null;
-
-			} else if ( lineNum === currentLineNumber ) {
-				lineNum--;
-
-			} else if ( /^\s*$/.test( line ) && lineNum > 1 ) {
-				lineNum--;
-
-			} else {
-				vscode.window.showErrorMessage('No Quartet file reference was found.');
+		// did we find anything?
+		if ( matches.length > 1 ) {
+			try {
+				const regX = eval( matches[ 0 ] );
+				if ( regX.constructor.name === "RegExp" )
+					// yes! it's a regX
+					return regX.source;
+			} catch ( e ) {
 				return null;
 			}
 		}
+		return null;
 	}
-
-//
-// ─── FS EXISTS ─────────────────────────────────────────────────────────────────
-//
-
-	function existsSync ( filename ) {
-        try {
-            fs.accessSync( filename );
-            return true;
-        } catch( ex ) {
-            return false;
-        }
-    }
-
 
 // ────────────────────────────────────────────────────────────────────────────────
